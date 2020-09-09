0a8669d1d1f859cf0c09a946952aa6fb6f4ef23d
Doggo <44546850+xdavidftw@users.noreply.github.com>
2020-09-09 14:07:53 +0200

    Add files via upload

---
+++ daddy.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+const prefix = '-';
+ 
+const fs = require('fs');
+ 
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
+ 
+ 
+client.once('ready', () => {
+    console.log('Codelyon is online!');
+    client.user.setActivity('Looking for Chikas', { type: 'WATCHING'}).catch(console.error);
+});
+ 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'fuck'){
+        client.commands.get('fuck').execute(message, args);
+    } 
+});
+
+client.login('BOT_TOKEN')
